7c8e2b57b81e909abba5284e0ecb6f7639dbf315
B00139870 Komal Wajahat <B00139870@mytudublin.ie>
2024-03-11 15:35:41 +0000

    second commit

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -1,20 +1,54 @@
 <?php
+
 namespace App\Http\Controllers;
-use App\Models\Member as Member;
+
+use App\Models\Member;
+use App\Models\Booking; // Don't forget to import the Booking model
 use Illuminate\Http\Request;
+
 class MemberController extends Controller
 {
     public function __construct()
     {
         //
     }
+
     public function showAllMembers()
     {
         $members = Member::all();
         return response()->json($members);
     }
+
     public function showOneMember($id)
     {
         return response()->json(Member::find($id));
+    }
+
+    public function showMemberBookings($id)
+    {
+        $bookings = Booking::where('memberid', $id)->get();
+        return response()->json($bookings);
+    }
+
+    public function create(Request $request)
+    {
+        $arr = $request->json()->all();
+        $member = new Member();
+        $member->forceFill($arr);
+        $member->save();
+        return response()->json($member, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $member = Member::findOrFail($id);
+        $member->update($request->all());
+        return response()->json($member, 200);
+    }
+
+    public function delete($id)
+    {
+        Member::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
     }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
+
 class Member extends Model
 {
     public function __construct($type = null)
@@ -8,24 +11,29 @@ class Member extends Model
         parent::__construct();
         $this->setTable("Member");
     }
-    public $timestamps=false;
+
+    public $timestamps = false;
+
     protected $fillable = [
         'firstname', 'surname', 'membertype', 'dateofbirth'
     ];
-    protected $hidden = [
-       'userid','created_at', 'updated_at', 'deleted_at'
-    ];
-}
 
-protected $appends = ['_links'];
-public function getLinksAttribute()
-{
-    return [
-        ['self' => app()->make('url')->to("api/members/{$this->attributes['id']}")],
-        ['bookings' => app()->make('url')->to("api/member/{$this->attributes['id']}/bookings")]
+    protected $hidden = [
+        'userid', 'created_at', 'updated_at', 'deleted_at'
     ];
-}
-public function bookings()
-{
-    return $this->hasMany(\App\Models\Booking::class, 'memberid');
+
+    protected $appends = ['_links'];
+
+    public function getLinksAttribute()
+    {
+        return [
+            ['self' => app()->make('url')->to("api/members/{$this->attributes['id']}")],
+            ['bookings' => app()->make('url')->to("api/member/{$this->attributes['id']}/bookings")]
+        ];
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(\App\Models\Booking::class, 'memberid');
+    }
 }
